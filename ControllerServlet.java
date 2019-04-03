95804a4d22e2e82423478d7e6d3f4c50b968e229
nemish1999 <47479394+nemish1999@users.noreply.github.com>
2019-04-03 13:22:53 +0530

    Add files via upload

---
+++ ControllerServlet.java
@@ -0,0 +1,93 @@
+package src;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class ControllerServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            ArrayList<Movie> t_movies=new ArrayList<>();
+            ArrayList<Movie> c_movies=new ArrayList<>();
+            Movie cm1= new Movie("comedy","cm1");
+            Movie cm2= new Movie("comedy","cm2");
+            Movie tm1= new Movie("thrill","tm1");
+            Movie tm2= new Movie("thrill","tm2");
+            t_movies.add(tm1);
+            t_movies.add(tm2);
+            
+            
+            c_movies.add(cm1);
+            c_movies.add(cm2);
+            String c=new String("comedy");
+            if(c.equals(request.getParameter("genre"))){
+                request.setAttribute("mymovies", c_movies);
+            }
+            else{
+                request.setAttribute("mymovies", t_movies);
+            }
+            RequestDispatcher rd = request.getRequestDispatcher("DisplayMoviesList.jsp");
+            rd.forward(request, response);
+            
+            
+            
+            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ControllerServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ControllerServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
